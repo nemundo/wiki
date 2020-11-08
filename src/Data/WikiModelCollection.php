ac5f74fc956e2632206df2f5248ffc060a96ad4d
unknown <urs.lang@gmail.com>
2020-11-08 21:03:05 +0100

    .

--- src/Data/WikiModelCollection.php
+++ src/Data/WikiModelCollection.php
@@ -1,7 +1,7 @@
 <?php
 namespace Nemundo\Wiki\Data;
 use Nemundo\Model\Collection\AbstractModelCollection;
-class WikiCollection extends AbstractModelCollection {
+class WikiModelCollection extends AbstractModelCollection {
 protected function loadCollection() {
 $this->addModel(new \Nemundo\Wiki\Data\WikiContent\WikiContentModel());
 $this->addModel(new \Nemundo\Wiki\Data\WikiPage\WikiPageModel());
--- src/Install/WikiInstall.php
+++ src/Install/WikiInstall.php
@@ -6,14 +6,13 @@ namespace Nemundo\Wiki\Install;
 
 use Nemundo\App\Script\Setup\ScriptSetup;
 use Nemundo\Content\App\Base\Collection\ContentAppContentTypeCollection;
-use Nemundo\Content\App\Base\Install\ContentAppInstall;
 use Nemundo\Content\Install\ContentInstall;
 use Nemundo\Content\Setup\ContentTypeSetup;
 use Nemundo\Model\Setup\ModelCollectionSetup;
 use Nemundo\Project\Install\AbstractInstall;
 use Nemundo\User\Setup\UsergroupSetup;
 use Nemundo\Wiki\Content\WikiPageContentType;
-use Nemundo\Wiki\Data\WikiCollection;
+use Nemundo\Wiki\Data\WikiModelCollection;
 use Nemundo\Wiki\Script\WikiCleanScript;
 use Nemundo\Wiki\Setup\WikiSetup;
 use Nemundo\Wiki\Usergroup\WikiEditorUsergroup;
@@ -29,7 +28,7 @@ class WikiInstall extends AbstractInstall
             ->install();
 
         (new ModelCollectionSetup())
-            ->addCollection(new WikiCollection());
+            ->addCollection(new WikiModelCollection());
 
         (new ContentTypeSetup())
             ->addContentType(new WikiPageContentType());
